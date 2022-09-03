bda0ff5d12647dc2a775e3630bc4e3d3d4f2d223
Angelo Breuer <angelobr@outlook.de>
2022-09-03 22:25:02 +0200

    feat: Add sample

---
+++ src/Localtunnel.Sample/Program.cs
@@ -0,0 +1,24 @@
+﻿using Localtunnel;
+using Localtunnel.Endpoints.Http;
+using Localtunnel.Handlers.Kestrel;
+using Localtunnel.Processors;
+using Microsoft.Extensions.Logging;
+
+var loggerFactory = LoggerFactory.Create(x => x.AddConsole().SetMinimumLevel(LogLevel.Trace));
+
+using var client = new LocaltunnelClient(loggerFactory);
+
+var pipeline = new HttpRequestProcessingPipelineBuilder()
+    .Append(new HttpHostHeaderRewritingRequestProcessor("test.angelobreuer.de"))
+    .Build();
+
+var endpointFactory = new HttpsTunnelEndpointFactory("test.angelobreuer.de", 443);
+var tunnelConnectionHandler = new KestrelTunnelConnectionHandler(pipeline, endpointFactory);
+
+var tunnel = await client
+    .OpenAsync(tunnelConnectionHandler)
+    .ConfigureAwait(false);
+
+await tunnel.StartAsync().ConfigureAwait(false);
+
+await Task.Delay(-1);
